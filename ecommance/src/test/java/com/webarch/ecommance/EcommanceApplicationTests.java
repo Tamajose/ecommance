5dcc74ff67bc125bde99e5576a7ee081a33746a9
Tamajose <tnvrmh41@gmail.com>
2026-08-06 19:48:54 +0600

    initial setup

---
+++ ecommance/src/test/java/com/webarch/ecommance/EcommanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webarch.ecommance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
