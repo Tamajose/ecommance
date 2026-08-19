7cb9c35c34f5a6b7a1a40d225168a3e305aa000a
Juhan Ahmed <ahmadjarif2004@gmail.com>
2026-08-20 01:26:48 +0600

    temporary role created for local testing

--- product-service/src/main/java/com/webarch/product/config/SecurityConfig.java
+++ product-service/src/main/java/com/webarch/product/config/SecurityConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +17,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails admin = User.builder()
+                                .username("admin")
+                                .password(passwordEncoder.encode("admin"))
+                                .roles("ADMIN")
+                                .build();
+        
+        return new InMemoryUserDetailsManager(admin);
     }
 
     @Bean
