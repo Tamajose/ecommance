fcf0785da77904a738b6f83449317f2d7da0963c
Arceus - 221 <aakash02.bd@gmail.com>
2026-08-22 12:55:30 +0600

    added UpdateQuantity DTO

---
+++ cart-service/src/main/java/com/webarch/cart/dto/UpdateQuantityRequest.java
@@ -0,0 +1,11 @@
+package com.webarch.cart.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateQuantityRequest(
+    @NotNull
+    @Min(1)
+    Integer quantity
+) {
+}
