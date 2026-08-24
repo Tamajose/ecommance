fd71fb325fc8633a206c37ad4bc2c181dcfe7050
Juhan Ahmed <ahmadjarif2004@gmail.com>
2026-08-24 17:44:48 +0600

    added product-api

---
+++ frontend/src/api/productApi.js
@@ -0,0 +1,44 @@
+import { API_URLS, request } from "./apiClient";
+
+export async function getProducts({ name, category} = {}){
+    const params = new URLSearchParams;
+
+    if(name){
+        params.append("name", name);
+    }
+
+    if(category){
+        params.append("category", category);
+    }
+
+    const query = params.toString();
+
+    return request(
+        API_URLS.product,
+        `/api/products${query ? `?${query}` : ""}`
+    );
+}
+
+export async function getProductById(id){
+    return request(API_URLS.product, `/api/products/${id}`);
+}
+
+export async function createProduct(product){
+    return request(API_URLS.product, "/api/products", {
+        method: "POST",
+        body: JSON.stringify(product)
+    });
+}
+
+export async function updateProduct(id, product){
+    return request(API_URLS.product, `/api/products/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(product)
+    });
+}
+
+export async function deleteProduct(id){
+    return request(API_URLS.product, `/api/products/${id}`, {
+        method: "DELETE"
+    });
+}
